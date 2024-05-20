3380ecf72f2cda77b7c7e73b5067aeeb83cba91d
lexcoto <mcoto@uwo.ca>
2024-05-20 19:08:15 -0400

    Add files via upload

--- Python Tutorial Folder/second submodule/Module Assessments/First Module assessment/main.py
+++ Python Tutorial Folder/second submodule/Module Assessments/First Module assessment/main.py
@@ -1,1 +1,7 @@
-
+def main ():
+    start_year= int(input("Enter your year of birth:"))
+    end_year= int(input("Enter the current year:"))
+    age= end_year - start_year
+    
+    print(age)
+main ()
