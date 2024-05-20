c6e3f4034e34b5059165945e98b7c9d172b465b4
lexcoto <mcoto@uwo.ca>
2024-05-20 18:25:08 -0400

    Add files via upload

--- Python Tutorial Folder/first submodule/Module Assessments/First Module assessment/first.py
+++ Python Tutorial Folder/first submodule/Module Assessments/First Module assessment/first.py
@@ -1,6 +1,6 @@
 def main ():
     print("Name: Marco (Alex) Coto")
     print ("Age:31")
-    print ("Summary of what you want to learn in this project: I'd like to learn how use github")
+    print ("Summary of what you want to learn in this project: I'd like to learn how to use github.")
 
 main ()
