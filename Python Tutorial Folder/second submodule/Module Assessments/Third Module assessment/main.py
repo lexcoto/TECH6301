7371b7a80e1c3db65cbefb02c8e91cbe15fa8849
lexcoto <mcoto@uwo.ca>
2024-06-01 23:40:42 -0400

    Add files via upload

--- Python Tutorial Folder/second submodule/Module Assessments/Third Module assessment/main.py
+++ Python Tutorial Folder/second submodule/Module Assessments/Third Module assessment/main.py
@@ -1,1 +1,13 @@
 
+import math
+def main ():
+    radius= int(input("Enter the radius:"))
+    circuference= 2*math.pi*radius
+    area= math.pi*(radius**2)
+    
+    
+    
+    print("Circumference:", circuference)
+    print("Area:", area)
+  
+main ()
